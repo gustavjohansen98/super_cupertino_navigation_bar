05925167d10dba86270c217cd9ad81cbc4d92315
gustavjohansen98 <gustavlj@live.dk>
2025-06-20 14:52:41 +0200

    exposing store to package clients

--- lib/super_cupertino_navigation_bar.dart
+++ lib/super_cupertino_navigation_bar.dart
@@ -6,4 +6,5 @@ export 'models/super_large_title.model.dart';
 export 'models/super_search_bar.model.dart';
 export 'models/super_search_bar_action.model.dart';
 export 'src/super_cupertino_navigation_bar.dart';
+export 'utils/store.dart' show Store, SearchBarAnimationStatus;
 export 'super_cupertino_navigation_bar.dart';
